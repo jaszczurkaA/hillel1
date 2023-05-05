d03189e30acb567b82591aca37fec86230f49341
jaszczurkaA <asenka.s.ua@gmail.com>
2023-05-05 08:17:28 +0300

    answer for third task

---
+++ lesson_3_hw_3.py
@@ -0,0 +1,36 @@
+a = input(str())
+
+parts =a.split('*')
+print('Name:' + parts[0])
+born = parts[1]
+deth = parts[2]
+b = int(born[0:4])
+c = int(deth[0:4])
+
+print('Age:')
+print(c - b)
+print('years')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
