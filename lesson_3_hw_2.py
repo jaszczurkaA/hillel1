b3f2f3c4957c974da0aff520afb951bca65747c5
jaszczurkaA <asenka.s.ua@gmail.com>
2023-05-09 12:15:52 +0300

    answer for second task corrected

--- lesson_3_hw_2.py
+++ lesson_3_hw_2.py
@@ -1,7 +1,13 @@
 snake = input()
 
-camel = snake.split()
+camel = snake.split('_')
+camel1 = camel[0].capitalize()
+camel2 = camel[1].capitalize()
+camel3 = camel[2].capitalize()
+camel = (camel1 + camel2+ camel3)
 camel = ''.join(camel)
 print(camel)
+
+
 
 
