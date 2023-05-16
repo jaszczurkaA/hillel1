f8464ee9032f2522f5616c959619f747abb5a134
jaszczurkaA <asenka.s.ua@gmail.com>
2023-05-16 15:23:57 +0300

    Homework lesson 5 №3

--- HW5/HW5_3.py
+++ HW5/HW5_3.py
@@ -1,12 +1,4 @@
-#lst1= input()
-#lst= list(map(int, input(lst1)))
 
-#for i in range(len(lst)):
- #   if lst[i]==0:
-      #  lst = lst.pop(i)
-      #  print(lst)
-      #  lst.append(int(input(i)))
-#print(lst)
 
 lst = list(map(int, input()))
 
