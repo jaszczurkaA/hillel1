84d6fbf6885c11e0ca17d05afa6d89337439bdf3
jaszczurkaA <asenka.s.ua@gmail.com>
2023-05-23 08:50:18 +0300

    Homework 9

--- HW9.py
+++ HW9.py
@@ -1,7 +1,8 @@
 str_of_numbers  = input()
 
 def add_one():
-    number = int(str_of_numbers)
+
+    number = int(''.join(map(str, str_of_numbers)))
     number_plus_one = number + 1
     number_plus_one_str = str(number_plus_one)
     number_plus_one_lst = list(number_plus_one_str)
