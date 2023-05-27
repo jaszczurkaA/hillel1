ea1657d92dabbd069a9150157df5597a77eaa722
jaszczurkaA <asenka.s.ua@gmail.com>
2023-05-27 21:46:03 +0300

    Homework 11

--- HW11.py
+++ HW11.py
@@ -2,17 +2,33 @@
 def find_unique_value(numbers):
     if not numbers:
         return None
-    unique = numbers[0]
-    for element in numbers:
-        if element in numbers:
-            continue
-        else:
-            clear(unique)
-            append.unique(element)
-    return unique
+    unique = []
+    for x in range(len(numbers)):
+        count_x = 0
+        for y in range(x+1, len(numbers)):
+            count_y  = 0
 
-#a = find_unique_value([1, 2, 1, 1])
-#print(a)
+            if numbers[x] == numbers[y]:
+                count_x += 1
+                continue
+            else:
+                count_y +=1
+            if count_y >count_x:
+               unique = numbers[x]
+            else:
+                unique = numbers[y]
+
+    return unique
+print(find_unique_value([3, 2, 3, 3, 3]))
+
+
+
+
+
+    # for element in numbers:
+    #     if element in unique:
+
+
 
 
 
